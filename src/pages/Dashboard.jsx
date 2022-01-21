ffd4f2e145ff750f894d92c4acc8c91269c62710
ehsan <ehsansaraee6570@yahoo.com>
2022-01-21 14:03:44 +0330

    login page added

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,11 +1,13 @@
 import { Info, Repos, User, Search, Navbar } from '../components';
-import loadingImage from '../images/preloader.gif';
-import { GithubContext } from '../context/context';
 
 const Dashboard = () => {
    return (
       <main>
-         <h2>Dashboard Page</h2>
+         <Navbar></Navbar>
+         <Search />
+         <Info />
+         <User />
+         <Repos />
       </main>
    );
 };
