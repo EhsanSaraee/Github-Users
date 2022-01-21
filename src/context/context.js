f41672ddd96fb7abe3fbc2ef74272aae8a0a1f3d
ehsan <ehsansaraee6570@yahoo.com>
2022-01-21 17:34:07 +0330

    mock data added to app

--- src/context/context.js
+++ src/context/context.js
@@ -1,12 +1,21 @@
-import { createContext, useContext } from 'react';
+import { createContext, useContext, useState } from 'react';
+import mockFollowers from './mockData.js/mockFollowers';
+import mockRepos from './mockData.js/mockRepos';
+import mockUser from './mockData.js/mockUser';
 
 const rootUrl = 'https://api.github.com';
 
 const GithubContext = createContext();
 
 const GithubProvider = ({ children }) => {
+   const [user, setUser] = useState(mockUser);
+   const [repos, setRepos] = useState(mockRepos);
+   const [followers, setFollowers] = useState(mockFollowers);
+
    return (
-      <GithubContext.Provider value="hello">{children}</GithubContext.Provider>
+      <GithubContext.Provider value={{ user, repos, followers }}>
+         {children}
+      </GithubContext.Provider>
    );
 };
 
