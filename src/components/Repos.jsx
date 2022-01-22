8bb241ea630f9cf72be3b28c4d3273a919006e07
ehsan <ehsansaraee6570@yahoo.com>
2022-01-22 12:25:21 +0330

    show chart in repos component

--- src/components/Repos.jsx
+++ src/components/Repos.jsx
@@ -1,8 +1,9 @@
 import styled from 'styled-components';
-import { GithubContext } from '../context/context';
+import { useGithubContext } from '../context/context';
 import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from './Charts';
 
 const Repos = () => {
+   const { repos } = useGithubContext();
    return <h2>repos component</h2>;
 };
 
