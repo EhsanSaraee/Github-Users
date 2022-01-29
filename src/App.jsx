39cb404c217178124a0d72dd95fb6560b0fa6ea1
ehsan <ehsansaraee6570@yahoo.com>
2022-01-29 19:21:58 +0330

    wrapping app with authWrapper

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,25 @@
-import { Dashboard, Error, Login, PrivateRoute } from './pages';
+import { AuthWrapper, Dashboard, Error, Login, PrivateRoute } from './pages';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 const App = () => {
    return (
-      <BrowserRouter>
-         <Routes>
-            <Route
-               path="/"
-               element={
-                  <PrivateRoute>
-                     <Dashboard />
-                  </PrivateRoute>
-               }
-            />
-            <Route path="/private" element={<PrivateRoute />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="*" element={<Error />} />
-         </Routes>
-      </BrowserRouter>
+      <AuthWrapper>
+         <BrowserRouter>
+            <Routes>
+               <Route
+                  path="/"
+                  element={
+                     <PrivateRoute>
+                        <Dashboard />
+                     </PrivateRoute>
+                  }
+               />
+               <Route path="/private" element={<PrivateRoute />} />
+               <Route path="/login" element={<Login />} />
+               <Route path="*" element={<Error />} />
+            </Routes>
+         </BrowserRouter>
+      </AuthWrapper>
    );
 };
 
