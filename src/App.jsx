9662bdb1225423d06811c195f2c554e9b5aeb36e
ehsan <ehsansaraee6570@yahoo.com>
2022-01-29 19:14:43 +0330

    private route added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
-import { Dashboard, Error, Login } from './pages';
+import { Dashboard, Error, Login, PrivateRoute } from './pages';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 const App = () => {
    return (
       <BrowserRouter>
          <Routes>
-            <Route path="/" element={<Dashboard />} />
+            <Route
+               path="/"
+               element={
+                  <PrivateRoute>
+                     <Dashboard />
+                  </PrivateRoute>
+               }
+            />
+            <Route path="/private" element={<PrivateRoute />} />
             <Route path="/login" element={<Login />} />
             <Route path="*" element={<Error />} />
          </Routes>
