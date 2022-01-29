412c3b62f671650d055c99d2f5c06ead3eccba85
ehsan <ehsansaraee6570@yahoo.com>
2022-01-29 13:53:19 +0330

    connecting app to auth0

--- src/index.jsx
+++ src/index.jsx
@@ -2,12 +2,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { GithubProvider } from './context/context';
+import { Auth0Provider } from '@auth0/auth0-react';
 
 const rootElement = document.getElementById('root');
 
 ReactDOM.render(
-   <GithubProvider>
-      <App />
-   </GithubProvider>,
+   <Auth0Provider
+      domain="dev-15gd455d.us.auth0.com"
+      clientId="jksdHavTBv1DHD7mBSbQClbHcOKSisyC"
+      redirectUri={window.location.origin}
+   >
+      <GithubProvider>
+         <App />
+      </GithubProvider>
+   </Auth0Provider>,
    rootElement
 );
